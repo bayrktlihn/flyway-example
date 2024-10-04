bb9ce991121a5471f53ca00106020757318ee523
alihan.bayraktar <alihan.bayraktar@icisleri.gov.tr>
2024-10-04 10:30:12 +0300

    first commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+create table person(
+    id bigserial primary key not null,
+    first_name varchar(255),
+    last_name varchar(255)
+);
---
+++ src/main/resources/db/migration/V2__init.sql
@@ -0,0 +1,4 @@
+insert into person(first_name, last_name) values ('alihan', 'bayraktar');
+insert into person(first_name, last_name) values('nihat', 'yüceyürek');
+insert into person(first_name, last_name) values('engin', 'çakır');
+insert into person(first_name, last_name) values('çağrı', 'çelik');
